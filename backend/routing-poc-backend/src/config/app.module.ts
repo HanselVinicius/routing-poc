78cb5862d59c7e6001b3994e75b745ba377e3c8a
Vinicius Hansel <hanelvinicius@gmail.com>
2025-05-19 12:43:45 -0300

    feat: db connection

--- backend/routing-poc-backend/src/config/app.module.ts
+++ backend/routing-poc-backend/src/config/app.module.ts
@@ -2,17 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from '../controller/app/app.controller';
 import { ProductController } from 'src/controller/product/product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { Product } from 'src/domain/Product';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT?.toString() ?? '5432') ,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Product],
       synchronize: true,
     }),
 
--- backend/routing-poc-backend/src/main.ts
+++ backend/routing-poc-backend/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './config/app.module';
 import "reflect-metadata"
+import { Logger } from '@nestjs/common';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule,{
+    logger: ['log','error','fatal','verbose']
+  });
+  const port = process.env.PORT;
+  Logger.log(`[Log] starting routing poc on port ${port} `)
+  await app.listen(port ?? 8080);
 }
 bootstrap();
